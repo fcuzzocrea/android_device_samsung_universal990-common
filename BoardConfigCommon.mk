f20e22fabfaecbb4ed5667bde65845bf042a956a
Stricted <info@stricted.net>
2018-09-21 23:36:26 +0200

    universal990: inherit Samsung LSI board support package makefiles
    Change-Id: I4799848b8b97c16926af556ad8ffed9607ff8964

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -119,3 +119,6 @@ BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+
+# Call Samsung LSI board support package
+include hardware/samsung_slsi-linaro/config/BoardConfig990.mk
