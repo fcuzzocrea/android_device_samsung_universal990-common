6dcef9049d16602ca6272ec6cdedbabb0a7ea02b
josip-k <d3hum4niz3d@gmail.com>
2021-01-17 21:35:49 +0100

    universal990: sensors: build samsung-multihal 2.1 service
    Signed-off-by: josip-k <d3hum4niz3d@gmail.com>
    Change-Id: Iced3c49d13a75afd4069263c321cf89c87339cbc

--- device-common.mk
+++ device-common.mk
@@ -259,7 +259,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.contexthub@1.0.vendor:64
+    android.hardware.contexthub@1.0.vendor:64 \
+    android.hardware.sensors-service.samsung-multihal
 
 # Shims
 PRODUCT_PACKAGES += \
