34090119a53155edaaf1d3580a1258f225af5616
Tim Zimmermann <tim@linux4.de>
2021-04-07 16:20:39 +0200

    universal9830: get access to all cameras
    * Possible camera IDs are from /vendor/etc/floating_feature.xml
    
    Change-Id: I5fb0fb57f5ea85c0c8ead5ddec2dba3cf0277f94
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -25,7 +25,7 @@ TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service_64 \
+    android.hardware.camera.provider@2.5-service_64.universal9830 \
     libhwjpeg \
     libsensorndkbridge
 
