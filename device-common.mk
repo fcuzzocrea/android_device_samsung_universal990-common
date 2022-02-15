79e261798807382e3de11cc734c2058448911e68
Robert Shih <robertshih@google.com>
2022-02-15 17:07:30 +0000

    universal990: enable AIDL DRM HALs
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: I431554dcbef014f8235f048ee062a218a2131f9c

--- device-common.mk
+++ device-common.mk
@@ -31,7 +31,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor:32
+    android.hardware.drm@1.3.vendor:32 \
+    android.hardware.drm-service.clearkey
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
