ba35f2088e318224b9b4859a461323307c56ff0d
Tim Zimmermann <tim@linux4.de>
2022-05-15 06:00:04 +0200

    universal9830: Add device FCM for samsung HALs
    Change-Id: I8e55e0356ca3abac073757a436c9eb7639a09b41
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -94,7 +94,8 @@ TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
 # Framework Matrix
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 
 # HIDL Manifest
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
