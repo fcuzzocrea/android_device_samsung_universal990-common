00a882057bf87f7ae2db5fa46dd44931b8f8db34
Tim Zimmermann <tim@linux4.de>
2022-09-09 15:24:33 +0200

    universal990: build AIDL memtrack HAL
    Change-Id: I3ae077b5f59e4fcf32489eef64bce8551d758998

--- device-common.mk
+++ device-common.mk
@@ -89,6 +89,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Memtrack
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack-service.samsung-mali
+
 # Neuralnetworks
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor \
